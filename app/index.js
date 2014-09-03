3ef2ced55989f88378ccbfd4e7268fb44aed85cd
shgtkshruch <shgtk.shruch@gmail.com>
2014-09-03 14:57:46 +0900

    Async install

--- app/index.js
+++ app/index.js
@@ -21,19 +21,25 @@ module.exports = yeoman.generators.Base.extend({
   },
 
   install: function() {
+    var _this = this;
+
+    function bundleInstall() {
+      if (!_this.options['skip-bundle']) {
+        _this.spawnCommand('bundle', ['install', '--path', 'vendor/bundle']);
+      }
+    }
+
     if (!this.options['skip-install']) {
       this.installDependencies({
         bower: false,
         npm: true,
-        skipInstall: this.options['skip-install']
+        skipInstall: this.options['skip-install'],
+        callback: function() {
+          bundleInstall();
+        }
       });
     }
-  },
-
-  end: function() {
-    if (!this.options['skip-bundle']) {
-      this.spawnCommand('bundle', ['install', '--path', 'vendor/bundle']);
-    }
   }
+
 });
 
