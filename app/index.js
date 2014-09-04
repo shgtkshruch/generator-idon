80018fd192350ad12be8b26b5e0a7e7577150fb2
shgtkshruch <shgtk.shruch@gmail.com>
2014-09-04 15:44:00 +0900

    Add prompt to JavaScript library
    Remove jQuery

--- app/index.js
+++ app/index.js
@@ -1,6 +1,34 @@
 var yeoman = require('yeoman-generator');
 
 module.exports = yeoman.generators.Base.extend({
+  prompting: function() {
+    var done = this.async();
+    this.prompt({
+      type: 'checkbox',
+      name: 'jslib',
+      message: 'Which do you use JavaScript Library?',
+      choices: [{
+        name: 'jQuery',
+        value: 'includeJquery',
+        checked: false
+      },{
+        name: 'Three.js',
+        value: 'includeThree',
+        checked: false
+      }]
+    }, function(answers) {
+
+      var hasjs = function(feat) {
+        return answers.jslib.indexOf(feat) !== -1;
+      }
+
+      this.includeJquery = hasjs('includeJquery');
+      this.includeThree = hasjs('includeThree');
+
+      done();
+    }.bind(this));
+  },
+
   git: function() {
     this.copy('gitignore', '.gitignore');
   },
@@ -14,6 +42,24 @@ module.exports = yeoman.generators.Base.extend({
   gulp: function() {
     this.copy('package.json', 'package.json');
     this.copy('gulpfile.coffee', 'gulpfile.coffee');
+  },
+
+  bower: function() {
+    var bower = {
+      name: this._.slugify(this.appname),
+      private: true,
+      dependencies: {}
+    };
+
+    if (this.includeJquery) {
+      bower.dependencies.jquery = '*';
+    }
+
+    if (this.includeThree) {
+      bower.dependencies['three.js'] = '*';
+    }
+
+    this.write('bower.json', JSON.stringify(bower, null, 2));
   },
 
   gem: function() {
@@ -31,7 +77,7 @@ module.exports = yeoman.generators.Base.extend({
 
     if (!this.options['skip-install']) {
       this.installDependencies({
-        bower: false,
+        bower: true,
         npm: true,
         skipInstall: this.options['skip-install'],
         callback: function() {
--- test/test.js
+++ test/test.js
@@ -2,7 +2,6 @@ var helpers = require('yeoman-generator').test;
 var assert = require('yeoman-generator').assert;
 var path = require('path');
 
-
 describe('CSS generator', function() {
   describe('Run test', function() {
 
@@ -20,18 +19,32 @@ describe('CSS generator', function() {
     });
 
     it('create expected files', function(done) {
-      runGen.withOptions(options).on('end', function() {
+      runGen.withOptions(options).withPrompt({jslib: ['includeJquery']})
+      .on('end', function() {
+
         assert.file(
           'src/index.jade',
           'src/styles/style.scss',
           'src/scripts/script.coffee',
           '.gitignore',
           'package.json',
+          'bower.json',
           'gulpfile.coffee'
         );
+
+        assert.fileContent([
+          ['bower.json', /jquery/],
+          ['bower.json', /"name": "tmp"/]
+        ]);
+
+        assert.noFileContent([
+          ['bower.json', /three\.js/]
+        ]);
+
         done();
       });
     });
 
   });
 });
+
