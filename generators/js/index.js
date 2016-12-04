386ee66a4dd71196d03552fc7a3ec47715d51bb2
shgtkshruch <shgtk.shruch@gmail.com>
2016-12-04 14:57:14 +0900

    Create page directory for JavaScript

--- generators/js/index.js
+++ generators/js/index.js
@@ -7,11 +7,25 @@ module.exports = generators.Base.extend({
   },
 
   prompting() {
+
+    const pages = fs.readdirSync('src')
+      .filter(file => file.indexOf('layout') === -1)
+      .filter(file => file.indexOf('.pug') > -1)
+      .map(pugFile => pugFile.replace(/\.pug/, ''));
+
+    pages.unshift('common');
+
     return this.prompt([{
+      type: 'list',
+      name: 'pageName',
+      message: 'Do you want to create a module of which page?',
+      choices: pages
+    },{
       type: 'input',
       name: 'scriptName',
       message: 'What is the name of the script you make?',
     }]).then(answers => {
+      this.pageName = answers.pageName;
       this.scriptName = answers.scriptName;
     });
   },
@@ -20,7 +34,7 @@ module.exports = generators.Base.extend({
     script() {
       this.copy(
         'script.js',
-        `src/scripts/${this.scriptName}.js`
+        `src/scripts/${this.pageName}/${this.scriptName}.js`
       );
     },
 
@@ -33,7 +47,7 @@ module.exports = generators.Base.extend({
 
       fs.writeFileSync(
         'src/layout.pug',
-        start + `script(src='scripts/${this.scriptName}.js')\n    ` + end
+        start + `script(src='scripts/${this.pageName}/${this.scriptName}.js')\n    ` + end
       );
     }
   },
