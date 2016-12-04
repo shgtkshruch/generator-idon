0c6cc2144b8ed4fd8cd68531b1447d3b250cc315
shgtkshruch <shgtk.shruch@gmail.com>
2016-12-04 14:43:46 +0900

    Modified src of JavaScript task

--- generators/app/templates/gulpfile.js
+++ generators/app/templates/gulpfile.js
@@ -80,7 +80,7 @@ gulp.task('sass', () => {
 });
 
 gulp.task('js', () => {
-  return gulp.src('src/scripts/*.js')
+  return gulp.src('src/scripts/**/*.js')
     .pipe($.plumber())
     .pipe($.changed('dist/scripts', {
       extension: '.js'
@@ -123,7 +123,7 @@ gulp.task('ghpages', () => {
 gulp.task('watch', () => {
   gulp.watch('src/**/*.pug', ['pug']);
   gulp.watch('src/styles/**/*.scss', ['sass']);
-  gulp.watch('src/scripts/*.js', ['js']);
+  gulp.watch('src/scripts/**/*.js', ['js']);
   gulp.watch('src/images/**/*', ['image']);
 });
 
